236f4b54589707e35912f6cf1a72482b3e6dde7c
wkennedy1786 <wkennedy@dublinschool.org>
2018-11-04 10:57:21 -0500

    Update __init__.py

--- RandGuess/check50/__init__.py
+++ RandGuess/check50/__init__.py
@@ -12,7 +12,3 @@ class RandGuess(Checks):
         """randguess.c compiles"""
         self.spawn("clang -o randguess randguess.c -lcs50 -lm").exit(0)
         
-    @check("compiles")
-    def test_name(self):
-        """input of Dr Bill yields output of Hi, Dr Bill!"""
-        self.spawn("./randguess").stdin("Dr Bill").stdout("Hi, Dr Bill!\n", "Hi, Dr Bill!\n").exit(0)
